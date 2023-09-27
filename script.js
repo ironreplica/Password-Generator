722f8467a43c6414e77495e59dfa0557f91f9c97
ironreplica <trevorchilds8@gmail.com>
2023-09-27 10:16:36 -0600

    Finished Password Generator

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-function generatePass() {
-  let length = parseInt(document.getElementById("length").value);
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowercase = "abcdefghijklmnopqrstuvwxyz"
+const symbols = "!@#$%^&*()_+{};~?,./";
+const numbers = "0123456789"
 
-  /* If uppercase */
-  /* If numbers */
-  /* If symbols */
+let password;
+
+function generatePass() {
+  password = '';
+  document.getElementById("password").value = "";
+
+  let length = parseInt(document.getElementById("length").value);
+  
+  let charset = lowercase;
+
+  if(document.getElementById("uppercase").checked){
+    charset+=uppercase;
+  }
+  if(document.getElementById("numbers").checked){
+    charset+=numbers;
+  }
+  if(document.getElementById("symbols").checked){
+    charset+=symbols;
+  }
+  for(let i = length; i > 0; i--){
+    password += charset[(Math.floor(Math.random()*charset.length))];
+  }
+  console.log(password);
+  document.getElementById("password").value = password;
+}
+function copyText(){
+  var passwordText = document.getElementById("password");
+  
+  passwordText.select();
+  passwordText.setSelectionRange(0, 99999);
+
+  navigator.clipboard.writeText(passwordText.value);
+  
 }
